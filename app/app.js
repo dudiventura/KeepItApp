6050cc84d16f2d311b5ebde24c82d13947e0178d
Shay Avraham <shay@picsize.co.il>
2016-01-04 18:09:16 +0200

    alerts

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-﻿
+﻿var keepItApp;
 var app = {
     init: function () {
         keepItApp = angular.module('keepItApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'angular-loading-bar']);
@@ -8,27 +8,32 @@ var app = {
         }
     },
     onDeviceReady: function () {
+        alert('onDeviceReady');
         this.getPhoneLanguage();
         this.getPhoneNumber();
         this.startApp();
     },
     getPhoneLanguage: function () {
+        alert('getPhoneLanguage');
         var onSuccess = function (res) { localStorage.setItem('lang', res.value.split('-')[0]); }
         var onFaild = function () { }
         navigator.globalization.getPreferredLanguage(onSuccess, onFaild);
     },
     getPhoneNumber: function () {
+        alert('getPhoneNumber');
         var onSuccess = function (res) { alert('your phone number is:\n' + res.phoneNumber); }
         var onFaild = function (error) { alert('Opps...:\n' + JSON.stringify(error)); }
         window.plugins.sim.getSimInfo(onSuccess, onFaild);
     },
     startApp: function () {
+        alert('startApp');
         var start = setInterval(function () {
+            alert('start');
             if (localStorage.getItem('lang') !== undefined) {
                 clearInterval(start);
                 angular.bootstrap(document, ['keepItApp']);
             }
-        }, 1000);
+        }, 500);
     }
 }
 
