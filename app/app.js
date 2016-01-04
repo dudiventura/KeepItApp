2a8ed48a982b814cd6891cad6d5fb0d0fc9c085d
Shay Avraham <shay@picsize.co.il>
2016-01-04 19:05:25 +0200

    66

--- app/app.js
+++ app/app.js
@@ -4,39 +4,31 @@ var app = {
         keepItApp = angular.module('keepItApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'angular-loading-bar']);
 
         if (app.isMobile()) {
-            alert('if')
             document.addEventListener('deviceready', app.onDeviceReady, false);
         }
         else {
-            alert('else');
             app.onDeviceReady();
         }
     },
     onDeviceReady: function () {
-        alert('onDeviceReady');
         if (app.isMobile()) {
-            alert('is mobile');
             app.getPhoneLanguage();
             app.getPhoneNumber();
         }
-        //app.startApp();
+        app.startApp();
     },
     getPhoneLanguage: function () {
-        alert('getPhoneLanguage');
         var onSuccess = function (res) { localStorage.setItem('lang', res.value.split('-')[0]); }
         var onFaild = function () { }
         navigator.globalization.getPreferredLanguage(onSuccess, onFaild);
     },
     getPhoneNumber: function () {
-        alert('getPhoneNumber');
         var onSuccess = function (res) { alert('your phone number is:\n' + res.phoneNumber); }
         var onFaild = function (error) { alert('Opps...:\n' + JSON.stringify(error)); }
         window.plugins.sim.getSimInfo(onSuccess, onFaild);
     },
     startApp: function () {
-        alert('startApp');
         var start = setInterval(function () {
-            alert('start');
             if (localStorage.getItem('lang') !== undefined) {
                 clearInterval(start);
                 angular.bootstrap(document, ['keepItApp']);
@@ -44,7 +36,6 @@ var app = {
         }, 500);
     },
     isMobile: function () {
-        alert('isMobile');
         return (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) ? true : false;
     }
 }
